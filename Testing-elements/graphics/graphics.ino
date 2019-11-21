dd21e4e0217cbcd4a794ff362fe616555b2bea0a
Aykut Koc <aykutkoc96@hotmail.com>
2019-11-21 16:28:07 +0100

    spi to i2c ofzo

--- Testing-elements/graphics/graphics.ino
+++ Testing-elements/graphics/graphics.ino
@@ -4,11 +4,17 @@
 #include "SH1106Ui.h"
 #include "emojis.h"
 
-#define OLED_RESET  D1   // RESET
-#define OLED_DC     D2   // Data/Command
-#define OLED_CS     D8   // Chip select
+//#define OLED_RESET  D1   // RESET
+//#define OLED_DC     D2   // Data/Command
+//#define OLED_CS     D8   // Chip select
 
-SH1106 display(true, OLED_RESET, OLED_DC, OLED_CS); // FOR SPI
+// Pin definitions for I2C
+#define OLED_SDA    D2  // pin 14
+#define OLED_SDC    D4  // pin 12
+#define OLED_ADDR   0x3C
+
+//SH1106 display(true, OLED_RESET, OLED_DC, OLED_CS); // FOR SPI
+SH1106   display(OLED_ADDR, OLED_SDA, OLED_SDC);
 SH1106Ui ui     ( &display );
 
 
