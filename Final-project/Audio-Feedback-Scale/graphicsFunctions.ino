29769d3e693ce571f60643bdfed09abf4a996248
Aykut Koc <aykutkoc96@hotmail.com>
2019-11-22 10:34:31 +0100

    Text change

--- Final-project/Audio-Feedback-Scale/graphicsFunctions.ino
+++ Final-project/Audio-Feedback-Scale/graphicsFunctions.ino
@@ -5,51 +5,39 @@ bool msOverlay1(SH1106 *display, SH1106UiState* state) {
   return true;
 }
 
-bool msOverlay2(SH1106 *display, SH1106UiState* state) {
+String printText(String text){
   display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->setFont(ArialMT_Plain_10);
-  display->drawStringMaxWidth(0, 15, 120 , getMood());
-  return true;
+  display->drawStringMaxWidth(0, 15, 120 , text);
 }
 
-String getMood(){
-  if (millis() < 10000) {
-    return "Nothing I can't manage!";
-  }
-  else if (millis() < 20000) {
-    return "I can do this";
-  }
-  else if (millis() < 30000) {
-    return "It's getting harder!";
-  }
-  else if (millis() < 40000) {
-    return "Get it off me you bastard!";
-  }
-  else {
-    return "AAAAAAAAAAAAAAAH!";
-  }
-}
-
-bool msOverlay3(SH1106 *display, SH1106UiState* state) {
+bool msOverlay2(SH1106 *display, SH1106UiState* state) {
   if (sensorFSR.outputMass == 100) {
+    printText("This is too easy for me");
     display->drawXbm(50, 30, Emoji1_width, Emoji1_height, Emoji1_bits);
   }
   else if (sensorFSR.outputMass == 200) {
+    printText("Nothing I can't manage!");
     display->drawXbm(50, 30, Emoji2_width, Emoji2_height, Emoji2_bits);
   }
   else if (sensorFSR.outputMass == 300) {
+    printText("It's getting harder, slow down");
     display->drawXbm(50, 30, Emoji3_width, Emoji3_height, Emoji3_bits);
   }
   else if (sensorFSR.outputMass == 400) {
+    printText("This is enough!");
     display->drawXbm(50, 30, Emoji4_width, Emoji4_height, Emoji4_bits);
   }
   else if (sensorFSR.outputMass == 500) {
+    printText("Get this off me you bastard!");
     display->drawXbm(50, 30, Emoji5_width, Emoji5_height, Emoji5_bits);
   }
   else if (sensorFSR.outputMass == 600) {
+    printText("Fuck off fat fuck!");
     display->drawXbm(50, 30, Emoji6_width, Emoji6_height, Emoji6_bits);
   }
   else if (sensorFSR.outputMass > 600) {
+    printText("AAAAAAAAAAAAAAAAAAAAAH");
     display->drawXbm(50, 30, Emoji7_width, Emoji7_height, Emoji7_bits);
   }
   else{
@@ -63,8 +51,8 @@ bool drawFrame(SH1106 *display, SH1106UiState* state, int x, int y) {
 }
 int frameCount = 0;
 bool (*frames[])(SH1106 *display, SH1106UiState* state, int x, int y) = {drawFrame};
-bool (*overlays[])(SH1106 *display, SH1106UiState* state)             = { msOverlay1, msOverlay2, msOverlay3 };
-int overlaysCount = 3;
+bool (*overlays[])(SH1106 *display, SH1106UiState* state)             = { msOverlay1, msOverlay2};
+int overlaysCount = 2;
 
 void setupUI() {
   ui.setTargetFPS(60);
